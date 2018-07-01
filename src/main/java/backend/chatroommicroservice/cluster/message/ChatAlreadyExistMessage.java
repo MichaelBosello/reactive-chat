e1edf54f41e9e67d409b6a5fb667aaeaa902b1df
Michael Bosello <michael.bosello@studio.unibo.it>
2018-07-01 17:26:39 +0200

    Some work

---
+++ src/main/java/backend/chatroommicroservice/cluster/message/ChatAlreadyExistMessage.java
@@ -0,0 +1,16 @@
+package backend.chatroommicroservice.cluster.message;
+
+import java.io.Serializable;
+
+public class ChatAlreadyExistMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final String id;
+
+    public ChatAlreadyExistMessage(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
---
+++ src/main/java/backend/chatroommicroservice/cluster/state/NewChatEvent.java
@@ -0,0 +1,16 @@
+package backend.chatroommicroservice.cluster.state;
+
+import java.io.Serializable;
+
+public class NewChatEvent implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final String id;
+
+    public NewChatEvent(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
